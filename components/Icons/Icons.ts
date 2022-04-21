2eb73a0c499e45e048f26a2f38b6f01af2b034d3
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-04-22 00:43:38 +0300

    icons ready

---
+++ components/Icons/Icons.ts
@@ -0,0 +1,25 @@
+import SunnyIcon from './svg/sunny.svg?sprite'
+import CloudyIcon from './svg/cloudy.svg?sprite'
+import ThunderstormsIcon from './svg/thunderstroms.svg?sprite'
+import NightRainIcon from './svg/nt-rain.svg?sprite'
+import RainIcon from './svg/rain.svg?sprite'
+import RainManuIcon from './svg/rain-many.svg?sprite'
+import SunnyRainIcon from './svg/sunny-rain.svg?sprite'
+import NightCloudyIcon from './svg/nt-cloudy.svg?sprite'
+import MoonIcon from './svg/moon.svg?sprite'
+import SunnyCloudyIcon from './svg/sunny-cloudy.svg?sprite'
+import CloudyWindIcon from './svg/cloudy-wind.svg?sprite'
+
+export {
+    SunnyIcon,
+    CloudyIcon,
+    ThunderstormsIcon,
+    NightRainIcon,
+    RainIcon,
+    RainManuIcon,
+    SunnyRainIcon,
+    NightCloudyIcon,
+    MoonIcon,
+    SunnyCloudyIcon,
+    CloudyWindIcon
+}
--- components/screens/Home/components/Card/Card.tsx
+++ components/screens/Home/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { CloudyIcon, CloudyWindIcon, NightCloudyIcon, NightRainIcon, RainIcon, RainManuIcon, SunnyCloudyIcon, SunnyIcon, SunnyRainIcon, ThunderstormsIcon } from '../../../../Icons/Icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { IWeather } from '../../../../../types/IWeather'
 import 'swiper/css';
@@ -9,14 +10,44 @@ interface IWeatherProps {
   item: IWeather
 }
 
+/* Простая switch case функция, которая генерирует иконку, на вход принимает пропс code (тут вся документация https://www.weatherapi.com/docs/#weather-icons) */
+
+const generateIcon = (code: number) => {
+  switch (code) {
+    case 1000:
+      return <SunnyIcon />
+    case 1003:
+      return <SunnyCloudyIcon />
+    case 1006:
+      return <NightCloudyIcon />
+    case 1009:
+      return <CloudyIcon />
+    case 1030:
+      return <CloudyWindIcon />
+    case 1276:
+      return <ThunderstormsIcon />
+    case 1063:
+      return <NightRainIcon />
+    case 1183:
+      return <RainIcon />
+    case 1189:
+        return <RainIcon />
+    case 1153:
+      return <RainManuIcon />
+    case 1240:
+      return <SunnyRainIcon />
+    default: return <CloudyIcon />
+  }
+}
+
 export const Card: FC<IWeatherProps> = ({ item }) => {
   return (
     <div className={styles.card}>
       <div className={styles.top}>
         <div className={styles.left}>
-          <img className={styles.image} src={item.current.condition.icon} />
+          {generateIcon(item.current.condition.code)}
           <h2 className={styles.title}>
-            {item.current.temp_c}
+            {Math.ceil(item.current.temp_c)}
             <sup>°C</sup>
           </h2>
           <ul className={styles.list}>
@@ -32,15 +63,15 @@ export const Card: FC<IWeatherProps> = ({ item }) => {
       </div>
       <Swiper
         className={styles.slider}
-        spaceBetween={15}
+        spaceBetween={20}
         slidesPerView='auto'
       >
         {item.forecast.forecastday.map(el =>
           el.hour.map((el, idx) => (
             <SwiperSlide style={{width: 'auto'}} key={idx}>
               <div className={styles.slide}>
-                <span className={styles.date}>{dayjs.unix(el.time_epoch).format("HH:mm")}</span>
-                <img className={styles.slideImage} src={el.condition.icon} />
+                <span className={styles.date}>{dayjs.unix(el.time_epoch).format("HH")}</span>
+                {generateIcon(el.condition.code)}
                 <span className={styles.temp}>{Math.ceil(el.temp_c)} <sup>°</sup></span>
               </div>
             </SwiperSlide>
