b112b3453632a1448c3080885b6e181380c47406
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-05-28 13:11:12 +0300

    some fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ export default function Document() {
           sizes="32x32"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#1e213a" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Head>
       <body>
